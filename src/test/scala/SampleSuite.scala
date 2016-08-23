2e782909fd2ee9e35e0bb15c4a07d37ede0ba5f9
heytitle <pat.chormai@gmail.com>
2016-08-23 15:48:31 +0700

    Add sample Test

---
+++ src/test/scala/SampleSuite.scala
@@ -0,0 +1,18 @@
+package sample;
+
+import java.util.concurrent._
+import scala.collection._
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import scala.math._
+import scala.collection.parallel._
+// import common._
+// import barneshut.conctrees.ConcBuffer
+
+@RunWith(classOf[JUnitRunner])
+class SampleSuite extends FunSuite {
+  test("samplesuite") {
+    assert( 1==1, "1 equal 1")
+  }
+}
